21288b4ccd2ed2155c39428009120f990b7f4393
unknown <ayandabuda73@gmail.com>
2024-02-19 22:25:44 +0200

    still not completed, just saving what i have now

--- OIBSIP_Task3/temp_converter.js
+++ OIBSIP_Task3/temp_converter.js
@@ -1,11 +1,17 @@
 
 function convertBtn() {
-    var temperature = document.getElementById("temperature").value;
+    var temperature = parseFloat(document.getElementById("temperature").value);
     var tempType = document.getElementById("convertTo").value;
 
     if(tempType == "Fahrenheit"){
-        var result = temperature * 33.8;
-        document.getElementById("result").textContent = "Result: " + result;
+        var result = (temperature * 9/5 ) + 32;
+        document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °F";
+        console.log("this is the temp: "+result);
+    }
+
+    else if(tempType == "Celsius"){
+        var result = (temperature -32) * 5/9;
+        document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °C";
     }
 
     
