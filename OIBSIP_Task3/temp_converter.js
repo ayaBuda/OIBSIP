033c7af1c667fd36f6479cc3d1820d4f42a2ca71
unknown <ayandabuda73@gmail.com>
2024-02-20 22:02:53 +0200

    still need to fix result appearance

--- OIBSIP_Task3/temp_converter.js
+++ OIBSIP_Task3/temp_converter.js
@@ -1,18 +1,38 @@
+console.log("hello world")
+const convertBtn = document.querySelector(".convertBtn");
 
-function convertBtn() {
-    var temperature = parseFloat(document.getElementById("temperature").value);
-    var tempType = document.getElementById("convertTo").value;
+convertBtn.addEventListener("click", () => {
+    const temperature = parseFloat(document.getElementById("temperature").value);
+    const tempType = document.getElementById("convertTo").value;
 
     if(tempType == "Fahrenheit"){
         var result = (temperature * 9/5 ) + 32;
-        document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °F";
+        document.getElementById("result").textContent = "Result: " + result.toFixed(2) + " °F";
+        // document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °F";
         console.log("this is the temp: "+result);
     }
 
     else if(tempType == "Celsius"){
-        var result = (temperature -32) * 5/9;
-        document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °C";
+        const result = (temperature -32) * 5/9;
+        document.getElementById("result").textContent = "Result: " + result.toFixed(2) + " °C";
+        // document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °C";
     }
+});
+
+// function convertBtn() {
+//     var temperature = parseFloat(document.getElementById("temperature").value);
+//     var tempType = document.getElementById("convertTo").value;
+
+//     if(tempType == "Fahrenheit"){
+//         var result = (temperature * 9/5 ) + 32;
+//         document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °F";
+//         console.log("this is the temp: "+result);
+//     }
+
+//     else if(tempType == "Celsius"){
+//         var result = (temperature -32) * 5/9;
+//         document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °C";
+//     }
 
     
-}
+// }
