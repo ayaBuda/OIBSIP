d38d9f4a996fed3f6c8c7e9db0fe1ebef8c18a51
unknown <ayandabuda73@gmail.com>
2024-02-24 17:22:18 +0200

    final product

--- OIBSIP_Task3/temp_converter.js
+++ OIBSIP_Task3/temp_converter.js
@@ -1,23 +1,3 @@
-// console.log("hello world")
-// const convertBtn = document.querySelector(".convertBtn");
-
-// convertBtn.addEventListener("click", () => {
-//     const temperature = parseFloat(document.getElementById("temperature").value);
-//     const tempType = document.getElementById("convertTo").value;
-
-//     if(tempType == "Fahrenheit"){
-//         var result = (temperature * 9/5 ) + 32;
-//         document.getElementById("result").textContent = "Result: " + result.toFixed(2) + " °F";
-//         // document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °F";
-//         console.log("this is the temp: "+result);
-//     }
-
-//     else if(tempType == "Celsius"){
-//         const result = (temperature -32) * 5/9;
-//         document.getElementById("result").textContent = "Result: " + result.toFixed(2) + " °C";
-//         // document.getElementById("outputResult").textContent = "Result: " + result.toFixed(2) + " °C";
-//     }
-// });
 
 function convertTemperature() {
   var convertFrom = document.getElementById("convertFrom").value;
@@ -29,8 +9,25 @@ function convertTemperature() {
   if (convertFrom === "celsius" && convertTo === "fahrenheit") {
     result = (temperature * 9 / 5) + 32;
     document.getElementById("result").innerHTML = temperature + "°C is " + result.toFixed(2) + "°F";
-  } else if (convertFrom === "fahrenheit" && convertTo === "celsius") {
+  }
+  else if (convertFrom === "celsius" && convertTo === "kelvin") {
+    result = temperature + 273.15;
+    document.getElementById("result").innerHTML = temperature + "°C is " + result.toFixed(2) + "K";
+  } 
+  else if (convertFrom === "fahrenheit" && convertTo === "celsius") {
     result = (temperature - 32) * 5 / 9;
     document.getElementById("result").innerHTML = temperature + "°F is " + result.toFixed(2) + "°C";
+  } 
+  else if (convertFrom === "fahrenheit" && convertTo === "kelvin") {
+    result = (temperature - 32) * 5 / 9 + 273.15;
+    document.getElementById("result").innerHTML = temperature + "°F is " + result.toFixed(2) + "K";
+  } 
+  else if (convertFrom === "kelvin" && convertTo === "celsius") {
+    result = temperature - 273.15;
+    document.getElementById("result").innerHTML = temperature + "K is " + result.toFixed(2) + "°C";
+  } 
+  else if (convertFrom === "kelvin" && convertTo === "fahrenheit") {
+    result = (temperature - 273.15) * 9 / 5 + 32;
+    document.getElementById("result").innerHTML = temperature + "K is " + result.toFixed(2) + "°F";
   }
 }
